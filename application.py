29fedfb7d5f8f8b2288238ec855f05a81e5f20f8
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-01 13:25:40 -0400

    modified changes to the app file

--- application.py
+++ application.py
@@ -2,12 +2,4 @@ print("Hello World")
 
 print("this is another statement")
 
-first_name = "Kojo "
-last_name = "Nketsia-Tabiri"
 
-name = first_name + last_name
-weight = 200
-
-sentence = name + " weighs " + str(300) + " lbs"
-
-print (sentence)
