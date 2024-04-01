a0a7b61ed176d411433453daaae263f3b329b2dd
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-01 15:41:27 -0400

    created arithmetic.py file

---
+++ arithmetic.py
@@ -0,0 +1,21 @@
+# num1 = 3
+# num2 =10
+# answer = num1/num2
+# print(answer)
+# data_type = type(answer)
+# print (data_type)
+
+# num1 = 3
+# num2 =10
+# answer = num1*num2
+# print(answer)
+
+# answer = (10 + 3) + (9 * 4)
+# print (answer)
+
+# answer = 10 % 3
+# print (answer) - modulus operator shows the remainder
+
+
+
+
