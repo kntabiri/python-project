16d58aea1fc7b10dd837d973b8a04c842584d5b1
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-02 20:34:42 -0400

    built a basic calculator

---
+++ app.py
@@ -0,0 +1,14 @@
+# Building a basic calculator
+
+num1 = input ("Enter a number")
+num2 = input ("Enter another number")
+# result = int (num1) - int (num2)
+result = float (num1) + float (num2)
+
+print(result)
+
+
+
+
+
+
