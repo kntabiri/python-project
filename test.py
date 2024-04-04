58420aa5d66532957b08b6a8d7edb47da655c21b
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-03 20:57:13 -0400

    add files

--- test.py
+++ test.py
@@ -1,18 +1,39 @@
-# string upper
-name = "kojo"
-print(name.upper())
+# # string upper
+# name = "kojo"
+# print(name.upper())
 
-# string lower
-name = "KOJO"
-print(name.lower())
+# # string lower
+# name = "KOJO"
+# print(name.lower())
 
-# string concat
-str1 = "Kojo"
-str2 = "Nketsia-Tabiri"
-result = str1 + " " + str2
-print (result)
+# # string concat
+# str1 = "Kojo"
+# str2 = "Nketsia-Tabiri"
+# result = str1 + " " + str2
+# print (result)
 
-# string length
-text = "Jesus loves me and you"
-length = len(text)
-print (length)
+# # string length
+# text = "Jesus loves me and you"
+# length = len(text)
+# print (length)
+
+# # string stripped
+# text = "   Some spaces around   "
+# stripped_text = text.strip()
+# print("Stripped text:", stripped_text)
+
+# # string replace
+# text = "Python is awesome!!"
+# new_text = text.replace("awesome!!", "great :)")
+# print("Modified text:", new_text)
+
+# text = "Satan is good!!"
+# new_text = text.replace ("Satan" , "God")
+# print("Modified text:", new_text)
+
+#string substring
+
+text = "Python is awesome"
+substring = "awesome"
+if substring in text:
+    print(substring, "found in the text")
