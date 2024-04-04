29d609ce9c259b38e11de748802fe2243e8195d6
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-03 20:34:40 -0400

    add test file

---
+++ test.py
@@ -0,0 +1,18 @@
+# string upper
+name = "kojo"
+print(name.upper())
+
+# string lower
+name = "KOJO"
+print(name.lower())
+
+# string concat
+str1 = "Kojo"
+str2 = "Nketsia-Tabiri"
+result = str1 + " " + str2
+print (result)
+
+# string length
+text = "Jesus loves me and you"
+length = len(text)
+print (length)
