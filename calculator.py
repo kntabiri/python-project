545d567195d698fdb7ee6e2721868b6dc139071c
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-04 21:07:38 -0400

    add calculator file

---
+++ calculator.py
@@ -0,0 +1,49 @@
+a = 100
+b = 20
+
+def addition():
+    # c = 200 local variable
+    print (a+b)
+
+def subtraction():
+    print(a-b)
+
+def multiplication():
+    print(a*b)
+
+def division():
+    print (a/b)
+
+addition()
+subtraction()
+multiplication()
+division()
+
+
+# # float variables
+
+# num1 = 10.0
+# num2 = 20.0
+
+# Basic Arithmetic
+
+# result1 = num1 + num2
+# print("Addition:", result1)
+
+
+# result2 = num1 - num2
+# print("Subtraction:", result2)
+
+
+# result3 = num1 * num2
+# print("Multiplication:", result3)
+
+# result4 = num1 / num2
+# print("Division:", result4)
+
+# result5 = round(3.98798427497, 5)
+# print("Rounding:", result5)
+
+
+
+
