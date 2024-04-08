97abbeb654fca348025960f91f1f2e8b4ed5c0ac
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-08 15:12:42 -0400

    add files to Day-01 folder

---
+++ Day-01/calculator.py
@@ -0,0 +1,21 @@
+a = 100
+b = 20
+
+# use of function
+
+def addition():
+    print (a+b)
+
+def subtraction():
+    print(a-b)
+
+def multiplication():
+    print(a*b)
+
+def division():
+    print (a/b)
+
+addition()
+subtraction()
+multiplication()
+division()
---
+++ Day-01/new_calculator.py
@@ -0,0 +1,7 @@
+# the import function calls out a module created
+
+import calculator
+calculator
+
+# how to import one function from a module
+# calculator.addition
