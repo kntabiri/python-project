605bff43ddfae6852f5aa02a762f58dbe805c646
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-10 20:34:18 -0400

    created an env.vars file

---
+++ Day-02/env-vars.py
@@ -0,0 +1,8 @@
+# how to create an environment variable for sensitive information
+
+import os
+print (os.getenv("password"))
+print (os.getenv("apitoken"))
+print (os.getenv("username"))
+
+# excecute python3 env.vars.py in the terminal
