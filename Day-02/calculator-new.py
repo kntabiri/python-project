510927e6ef3ed04cc6a4fc16337fd5e20e9ad66e
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-10 20:33:53 -0400

    modified file

--- Day-02/calculator-new.py
+++ Day-02/calculator-new.py
@@ -12,16 +12,16 @@ def mul (num1, num2):
     mul = num1 * num2 
     return mul
 
-# print(add (5,10))
-# print(sub(10,5))
-# print(mul(10,5))
+# # print(add (5,10))
+# # print(sub(10,5))
+# # print(mul(10,5))
 
-num1 = int(sys.argv[1])
+# num1 = int(sys.argv[1])
 operation = sys.argv[2]
 num2 = int(sys.argv[3])
 
 if operation == "sub":
-    output = sub(num2, num1)
+    output = sub(num2,num1)
     print(output)
 
 # python3 calculator-new.py 4 sub 1 to execute a command line arg
