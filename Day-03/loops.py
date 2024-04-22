a8d4b058771cf9319b7679e076cd3dc45b75438b
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-22 16:55:18 -0400

    add files

---
+++ Day-03/loops.py
@@ -0,0 +1,30 @@
+# for a in range (10):
+#     print (a)
+#     print ("-kojo")
+
+# for is based on a definite amount of repetitive action of the block of code
+# for variable in sequence (list,tuple,string,int):
+
+# while is based on the condition given before the block of code is executed
+# while condition :
+
+# colors = ["yellow", "green", "blue"]
+# for colors in range (100):
+#     print (colors)
+
+numbers = [1,2,3,4,5,6]
+for number in numbers:
+    if number == 4 :
+        break
+    print (number)
+
+# break is used to stop the execution of a loop process
+
+numbers = [1,2,3,4,5,6]
+for number in numbers:
+    if number == 1 :
+        continue
+    print (number)
+
+# continue is used to continue the execution of a loop process
+
