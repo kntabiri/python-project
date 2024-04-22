8f0ad095b70303085d3e1e9471210c6afb10c89e
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-22 12:20:57 -0400

    add some changes

--- Day-03/slicing.py
+++ Day-03/slicing.py
@@ -1,3 +1,13 @@
-s3_bucket_list = ("hey1","you","miss")
-new_list = s3_bucket_list[1:2]
-print(new_list)
+# s3_bucket_list = ("hey","you","miss")
+# new_list = s3_bucket_list[0:4]
+# print(new_list)
+
+# numbers = [10,20,50,30,70,88]
+# numbers.sort()
+# print(numbers)
+
+# this is the way to sort variables such as int or strings
+
+random_list = [1,30,50,"hallo", "justice", 9.00, "heaven"]
+print(random_list [4] + "  " +  random_list[3])
+# this is concencation
