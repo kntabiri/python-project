0c7137a4db7b993e6f48d82bcc94396f060677ff
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-22 12:05:34 -0400

    update changes

--- Day-03/test.py
+++ Day-03/test.py
@@ -4,12 +4,15 @@
 # the append takes only one argument per line
 
 
-s3_bucket_lists = ["kojobucket01", "2024kojobucket", "amazingbuckets124","batmanliveshere"]
-s3_bucket_lists.append("diaskfdhsa")
-print (s3_bucket_lists)
+# s3_bucket_lists = ["kojobucket01", "2024kojobucket", "amazingbuckets124","batmanliveshere"]
+# print (type(s3_bucket_lists))
+# s3_bucket_lists.append("diaskfdhsa")
+# print (s3_bucket_lists)
 # s3_bucket_lists.remove
 
 
 
 # tuples are immutable or can not be changed (good memory footprint)
-# )
+s3_bucket_lists = ("kojobucket01", "2024kojobucket", "amazingbuckets124","batmanliveshere")
+print (type(s3_bucket_lists))
+print(len(s3_bucket_lists))
