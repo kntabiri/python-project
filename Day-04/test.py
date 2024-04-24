fd9642d0cd44d44aa048100c1629ba4faed7f106
Kojo Nketsia-Tabiri <kntabiri@gmail.com>
2024-04-23 21:12:56 -0400

    boto3 file

---
+++ Day-04/test.py
@@ -0,0 +1,13 @@
+import boto3
+
+client = boto3.client('s3')
+
+response = client.create_bucket(
+    Bucket='kojo-s3-bucket-demo',
+)
+
+response = client.get_bucket_acl(
+    Bucket= 'kojo-s3-bucket-demo',
+)
+
+print(response)
